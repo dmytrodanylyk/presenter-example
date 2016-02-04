b0dcd14645e2dcb54b6caa7b3ded74bf7bdcacd4
Dmytro Danylyk <dmytrodanylyk@gmail.com>
2016-02-04 21:37:06 +0200

    Part of presenter logic

--- app/src/main/java/com/dd/MainActivity.java
+++ app/src/main/java/com/dd/MainActivity.java
@@ -1,16 +1,25 @@
 package com.dd;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MainPresenter mPresenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolBar();
+        initView();
+
+        mPresenter = new MainPresenter(this);
+        mPresenter.initPresenter();
     }
 
     @Override
@@ -22,10 +31,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_settings:
+                mPresenter.onToolbarSettingsClicked();
+                return true;
+            case R.id.action_help:
+                mPresenter.onToolbarHelpClicked();
+                return true;
+            case android.R.id.home:
+                mPresenter.onToolbarBackClicked();
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void initToolBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.title);
+        setSupportActionBar(toolbar);
+    }
+
+    private void initView() {
+
+    }
+
 }
